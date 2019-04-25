1e7b71c96c4b700e66977c2dfb775f1786db630d
Matthew LeFevre <matthewlefevre95@gmail.com>
2019-04-25 10:06:46 -0600

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Matthew LeFevre";
+            string location = "Idaho";
+
+            Console.WriteLine($"My name is  {name}");
+            Console.WriteLine($"and I live in { location}");
+
+            var today = DateTime.Today;
+
+            Console.WriteLine(today.ToString());
+
+            string daysLeft = "245";
+            Console.WriteLine($"There are {daysLeft} days left till christmas.");
+        }
+    }
+}
