835df8fe346149cd12825fc1f5c8cc0d617a7048
Matthew LeFevre <matthewlefevre95@gmail.com>
2019-04-25 10:38:45 -0600

    Finished Assignment

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -18,10 +18,32 @@ namespace ConsoleApplication
 
             var today = DateTime.Today;
 
-            Console.WriteLine(today.ToString());
+            Console.WriteLine(today.ToString("d"));
 
-            string daysLeft = "245";
+            DateTime christmas = new DateTime(today.Year, 12, 25);
+
+            string daysLeft = (christmas - today).TotalDays.ToString();
             Console.WriteLine($"There are {daysLeft} days left till christmas.");
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            Console.Write("What is the width of the window: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.Write("what is the area of the height of the window: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is " + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+             
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
